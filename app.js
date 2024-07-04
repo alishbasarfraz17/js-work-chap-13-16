152af54306b02b153cf269ee57c86d29f7ba4569
alishbasarfraz17 <154826045+alishbasarfraz17@users.noreply.github.com>
2024-07-05 00:27:09 +0500

    my-js-work

---
+++ app.js
@@ -0,0 +1,41 @@
+//    Task One
+
+var StudentsNameInFuture = [] ;
+document.write(StudentsNameInFuture)
+
+
+//  Task Two
+
+var studentNamesObject = new Array() ;
+document.write(studentNamesObject)
+
+// Task Three
+
+var stringsArray = ["Apple", "Banana", "Mango"] ;
+document.write(stringsArray )
+document.write("<br>")
+document.write("<br>")
+
+
+// Task Four
+
+var numbersArray = [1, 2, 3, 4, 5] ;
+document.write(numbersArray)
+document.write("<br>")
+
+// Task Five
+
+var booleanArray = [true, false, true, false] ;
+document.write("<br>")
+
+// Task Six
+
+var mixedPlate =  [3,false,"alishba",undefined,null,true,7]
+document.write(mixedPlate)
+document.write("<br>")
+
+// Task Seven
+
+
+// Task Eight
+
